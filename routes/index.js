7f9298277fb92da5104ca4f549932908711ea81a
dev-song <52951039+dev-song@users.noreply.github.com>
2020-08-23 23:16:37 +0900

    Style: add a classname for page title

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,13 @@ const template = require('../lib/template.js');
 router.get('/', (req, res) => {
   const title = 'Session Testing';
   const body = `
-    <h1>${title}</h1>
     <main role="main">
+      <h1 class="page-title">${title}</h1>
       <section class="section-1">
         <p>This is section 1</p>
       </section>
     </main>
-  `
+  `;
   const page = template.HTML(title, body);
 
   res.send(page);
