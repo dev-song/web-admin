c9c9067d147adcdb0aa169428e4415e64c563ac1
dev_song <52951039+dev-song@users.noreply.github.com>
2020-08-24 23:06:05 +0900

    Feat: index page now contains products list and data manager UI

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,14 @@ const auth = require('../lib/auth.js');
 
 router.get('/', (req, res) => {
   const title = 'Session Testing';
+  const list = template.product(req.products);
   const body = `
     <main role="main">
       <h1 class="page-title">${title}</h1>
-      <section class="section-1">
-        <p>This is section 1</p>
-      </section>
+      ${auth.isUser(req, res) ? list : ''}
     </main>
   `;
-  const page = template.HTML(title, body, auth.loginStatus(req, res));
+  const page = template.HTML(title, body, auth.loginStatus(req, res), auth.dataManagerUI(req, res));
 
   res.send(page);
 });
