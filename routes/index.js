e3244b095ab00d3d6f68dec80f83e605d9dee2db
dev_song <52951039+dev-song@users.noreply.github.com>
2020-08-27 00:01:35 +0900

    Update index.js
    Modify parameters to display index page

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,17 @@ const auth = require('../lib/auth.js');
 
 router.get('/', (req, res) => {
   const title = 'Session Testing';
-  const list = template.product(req.products);
+  const list = template.products(req.products);
   const body = `
     <main role="main">
       <h1 class="page-title">${title}</h1>
       ${auth.isUser(req, res) ? list : ''}
     </main>
   `;
-  const page = template.HTML(title, body, '', auth.loginStatus(req, res), auth.dataManagerUI(req, res));
+  const header = `
+    <a href='/admin'>관리자</a>
+  `
+  const page = template.HTML(title, body, header, '');
 
   res.send(page);
 });
