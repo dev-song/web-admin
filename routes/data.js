042e4dac4743ccf44c406ff97d247238313a3987
dev_song <52951039+dev-song@users.noreply.github.com>
2020-08-29 00:34:44 +0900

    Feat: add a form to upload data in data.js

--- routes/data.js
+++ routes/data.js
@@ -4,11 +4,27 @@ const template = require('../lib/template.js');
 const auth = require('../lib/auth.js');
 
 router.get('/', (req, res) => {
-  if (!auth.isUser(req, res)) {
-    res.redirect('/');
-  }
+  const pageTitle = 'Upload Item';
+  const body = auth.isUser(req, res) ? `
+    <main role='main'>
+      <form class='upload-item' action='/data/upload-process' method='post'>
+        <h1 class='upload-item__title'>Upload Item</h1>
+        <input class='upload-item__building' type='number' name='building' placeholder='동 번호' />
+        <input class='upload-item__space' type='number' name='space' placeholder='평형' />
+        <input class='upload-item__description' type='text' name='description' placeholder='설명' />
+        <input class='upload-item__submit' type='submit' value='Upload' />
+      </form>
+    </main>
+  ` : '';
+  const header = auth.isUser(req, res) ? `
+    <header class='header-data'>
+      <p class='header__greetings'>OOO</p>
+      <a class='header__home' href="/">Home</a>
+    </header>
+  ` : '';
+  const page = template.HTML(pageTitle, body, header);
 
-  res.send('data page');
+  res.send(page);
 })
 
 module.exports = router;
