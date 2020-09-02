614e01e4bc10031b54ccbf62b95de2181d5bd7b7
dev-song <52951039+dev-song@users.noreply.github.com>
2020-09-02 21:16:40 +0900

    Feat: user can upload text and data to the server

--- routes/data.js
+++ routes/data.js
@@ -2,12 +2,20 @@ const express = require('express');
 const router = express.Router();
 const template = require('../lib/template.js');
 const auth = require('../lib/auth.js');
+const data = require('../lib/data.js');
 const multer = require('multer');
 const upload = multer({ dest: 'data/uploads/' });
 
+const DATA_PATH = './data/product-data.json';
+
 router.get('/', (req, res) => {
+  if (!auth.isUser(req, res)) {
+    res.redirect('/admin');
+    return;
+  }
+
   const pageTitle = 'Upload Item';
-  const body = auth.isUser(req, res) ? `
+  const body = `
     <main role='main'>
       <form class='upload-item' action='/upload-data/process' method='post' enctype='multipart/form-data'>
         <h1 class='upload-item__title'>Upload Item</h1>
@@ -20,25 +28,41 @@ router.get('/', (req, res) => {
         <input class='upload-item__submit' type='submit' value='Upload' />
       </form>
     </main>
-  ` : '';
-  const header = auth.isUser(req, res) ? `
+  `;
+  const header = `
     <header class='header-data'>
       <p class='header__greetings'>OOO</p>
       <a class='header__link-home' href="/">Home</a>
     </header>
-  ` : '';
+  `;
   const script = './scripts/script.js';
   const page = template.HTML(pageTitle, body, header, script);
 
   res.send(page);
 })
 
-router.post('/process', upload.single('images'), (req, res) => {
+router.post('/process', upload.array('images'), (req, res) => {
   const { building, space, description } = req.body;
-  const id = new Date().getTime();
-  const cluster = Math.floor(parseInt(req.body.building) / 100).toString();
+  const imagesInfo = req.files.map(elm => {
+    return {
+      name: elm.originalname,
+      type: elm.mimetype,
+      fileName: elm.filename
+    };
+  })
 
-  console.log(req.file);
+  const dataToUpload = {
+    id: new Date().getTime(),
+    info: {
+      cluster: Math.floor(parseInt(req.body.building) / 100),
+      building,
+      space,
+      description,
+      imagesInfo
+    }
+  };
+
+  data.saveImageData(DATA_PATH, dataToUpload);
 })
 
 module.exports = router;
